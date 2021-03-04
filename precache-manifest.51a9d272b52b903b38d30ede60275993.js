b6b2b667e3c250c4c73e826d02b7ad1ca17cf0a8
Brullrr <rbrull@yahoo.com>
2021-03-04 23:09:25 +0900

    Updates

---
+++ precache-manifest.51a9d272b52b903b38d30ede60275993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff1e17fd942681cdc9d318c5a45ff47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "622a488db703c29dd03e",
+    "url": "/static/css/main.af084074.chunk.css"
+  },
+  {
+    "revision": "f0275c31d94125c87df0",
+    "url": "/static/js/2.e90dddae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e90dddae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622a488db703c29dd03e",
+    "url": "/static/js/main.e2a23bb7.chunk.js"
+  },
+  {
+    "revision": "7c32ca8f8e809d015da3",
+    "url": "/static/js/runtime-main.943f7be6.js"
+  },
+  {
+    "revision": "e1c9f1c1957b64196062b731bb598ded",
+    "url": "/static/media/Burger-Menu.e1c9f1c1.PNG"
+  }
+]);
