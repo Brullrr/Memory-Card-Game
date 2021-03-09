3d1b52493b2783aaa4d801d6f949313e91014f8b
Brullrr <rbrull@yahoo.com>
2021-03-09 20:37:22 +0900

    Continued with third installment

--- src/MemoryCardGame/MemoryCardGame.js
+++ src/MemoryCardGame/MemoryCardGame.js
@@ -23,16 +23,7 @@ const MemoryCardGame = (props) => {
     <Fragment>
         {overlay}
         {firstTime}
-        <div>
-            <p>a world map with four major clickable sections</p>
-            <p>each section is set to clickable after completing the prior stage</p>
-            <p>LOL I AM ROTFL</p>
-            <p>Components that need to be added</p>
-            <p>a component for each "level"</p>
-            <p>I have 0 direction of where this project is headed</p>
-            <p>list characters</p>
-            <p>Should i use routing (browser router or Nextjs) for different stages or just a popup????</p>
-        </div>
+        
         <WorldMap />
     </Fragment> 
     )
--- src/MemoryCardGame/components/WorldMapComponent/WorldMap.js
+++ src/MemoryCardGame/components/WorldMapComponent/WorldMap.js
@@ -6,10 +6,22 @@ const WorldMap = () => {
     return (
         <Fragment>
             <div className={classes.WorldMap}>
-                <div>Meet the tttown?</div>
-                <div>learn The negihbors</div>
-                <div>Kill the invaders</div>
-                <div>Become the invading aliens</div>
+                <div className={classes.StageOne}>Meet the tttown?</div>
+
+                <div className={classes.StageTwo}>learn The negihbors</div>
+
+                <div className={classes.StageThree}>Kill the invaders</div>
+
+                <div className={classes.StageFour}>Become the invading aliens
+                    <p>a world map with four major clickable sections</p>
+                    <p>each section is set to clickable after completing the prior stage</p>
+                    <p>LOL I AM ROTFL</p>
+                    <p>Components that need to be added</p>
+                    <p>a component for each "level"</p>
+                    <p>I have 0 direction of where this project is headed</p>
+                    <p>list characters</p>
+                    <p>Should i use routing (browser router or Nextjs) for different stages or just a popup????</p>
+                </div>
             </div>
         </Fragment>
     )
